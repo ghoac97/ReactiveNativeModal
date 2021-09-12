eac3aa42338f70d2dafe0945f22c89278530e11d
ghoac97 <49738292+ghoac97@users.noreply.github.com>
2021-09-12 19:37:06 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -56,10 +56,8 @@ const getCurrentDate=()=>{
       var date = new Date().getDate();
       var month = new Date().getMonth() + 1;
       var year = new Date().getFullYear();
-
-      //Alert.alert(date + '-' + month + '-' + year);
-      // You can turn it in to your desired format
-      return date + '-' + month + '-' + year;//format: dd-mm-yyyy;
+      
+      return date + '-' + month + '-' + year;
 }
 
 const DATA = [
